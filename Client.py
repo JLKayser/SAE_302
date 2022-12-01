a9f4435cd0015eb1a61ac93831b0e2f1f526e96f
JLKayser <92865016+JLKayser@users.noreply.github.com>
2022-12-01 11:35:20 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,92 +1,137 @@
 from socket import AF_INET, SOCK_STREAM , socket
 from threading import Thread
 import sys
-'''from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QMainWindow, QComboBox, QDialog, QMessageBox , QTabWidget , QVBoxLayout
-from PyQt5.QtCore import QCoreApplication'''
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QMainWindow, QComboBox, QDialog, QMessageBox , QTabWidget , QVBoxLayout
+from PyQt5.QtCore import QCoreApplication
 
-'''class MainWindow(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         #self.__aide = None
+
         widget = QWidget()
         self.resize(450,300)
         self.setCentralWidget(widget)
         grid = QGridLayout()
         widget.setLayout(grid)
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.resize(300,300)
-        self.tabs.addTab(self.tab1,"Server")
-        self.tabs.addTab(self.tab2,"Client")
+        self.__tabs = QTabWidget()
+        self.__tab1 = QWidget()
+        self.__tab2 = QWidget()
+        self.__tab3 = QWidget()
+        self.__tabs.resize(300,300)
+
+        self.__tabs.addTab(self.__tab1,"Server")
+        self.__tabs.addTab(self.__tab2,"Client")
+        self.__tabs.addTab(self.__tab3,"Server_msg")
+
         #self.tabs.setStyleSheet("QWidget { background-color: black }")
-        self.tab1.layout = QGridLayout()
-        self.pushCommand = QLineEdit("")
-        self.connect = QPushButton('Connect')
-        self.tab1.layout.addWidget(self.pushCommand)
-        self.tab1.setLayout(self.tab1.layout)
-        grid.addWidget(self.tabs, 0, 0)
-        grid.addWidget(self.pushCommand, 3, 0)
-        self.tab1.layout.addWidget(self.connect, 2 ,0)
-        self.__convertir.clicked.connect(self.__ConversionCK)
-        self.__help.clicked.connect(self.__Help)
+        self.__tab1.layout = QGridLayout()
+        self.__tab2.layout = QGridLayout()
+        self.__tab3.layout = QGridLayout()
+        self.__pushCommand = QLineEdit("")
+        self.__pushCommand.setPlaceholderText("Type an command...")
+        self.__addressIP = QLineEdit("")
+        self.__addressIP.setPlaceholderText("Type an IP address...")
+        self.__port = QLineEdit("")
+        self.__port.setPlaceholderText("Type an port...")
+        self.__connect = QPushButton('Connect')
+        self.__tab1.layout.addWidget(self.__pushCommand)
+        self.__tab1.setLayout(self.__tab1.layout)
+        #self.connect.setStyleSheet("border-radius:5px;background-color:black;color:white;height:20px;width:40px;")
+        self.__label = QLabel('->')
+        self.__tab3.layout.addWidget(self.__label)
+        self.__tab3.setLayout(self.__tab3.layout)
+
+        grid.addWidget(self.__tabs, 0, 0)
+        grid.addWidget(self.__pushCommand, 3, 0)
+        self.__tab1.layout.addWidget(self.__connect, 2, 0)
+        self.__tab1.layout.addWidget(self.__addressIP, 1, 0)
+        self.__tab1.layout.addWidget(self.__port, 1, 1)
+
+
+
+        '''self.__convertir.clicked.connect(self.__ConversionCK)
+        self.__help.clicked.connect(self.__Help)'''
+        self.__connect.clicked.connect(self.__connection)
         self.setWindowTitle("SAE-302")
-    def __Help(self):
+
+    '''def __Help(self):
         msg = QMessageBox()
         msg.setWindowTitle("Aide")
         msg.resize(500, 500)
         msg.setIcon(QMessageBox.Information)
         msg.setText("Permet de convertir un nombre soit de Kelvin vers Celcuis, soit de Celcuis vers Kelvin.")
-        msg.exec()
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec()'''
+        msg.exec()'''
 
-HOST = '127.0.0.1'
-PORT = 5500
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((HOST,PORT))
-client_socket.sendall(bytes("This is from Client",'UTF-8'))
 
-def msg_send():
-    global client_socket
+    def __connection(self):
+        HOST = self.__addressIP.text()
+        PORT = int(self.__port.text())
+        self.socket = connect(HOST,PORT)
+        receive_thread = Thread(target=recv_msg, args=[self.socket])
+        thread_send = Thread(target=msg_send , args=[self.socket])
+        receive_thread.start()
+        thread_send.start()
+
+
+    def __message_recu(self):
+        pass
+
+
+
+
+def connect(HOST:str,PORT:int):
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((HOST,PORT))
+    client_socket.sendall(bytes("This is from Client",'UTF-8'))
+    Thread(target=recv_msg, args=[client_socket])
+    return client_socket
+
+
+def msg_send(connection):
     user = input('Enter your username : ')
     while True:
         try:
             msg = input(f'{user}-> ')
             if msg == 'reset':
-                client_socket.close()
+                connection.close()
                 client_socket = socket(AF_INET, SOCK_STREAM)
-                client_socket.connect((HOST, PORT))
-                client_socket.sendall(bytes("This is from Client", 'UTF-8'))
-                msg_send()
+                #new_connection(connection.getpeername())
+                #client_socket.connect((HOST, PORT))
+                #client_socket.sendall(bytes("This is from Client", 'UTF-8'))
+                #msg_send()
             if msg != 'disconnect':
-                client_socket.send(msg.encode())
+                connection.send(msg.encode())
             else:
-                disconnect()
+                disconnect(connection)
         except EOFError:
-            disconnect()
+            disconnect(connection)
 
 
-def recv_msg():
+def new_connection():
+    pass
+
+
+def recv_msg(connection):
     while True:
         try:
-            msg = client_socket.recv(1024).decode()
+            msg = connection.recv(1024).decode()
             print(msg)
         except:
             pass
 
-def disconnect():
-    client_socket.send('disconnect'.encode())
-    client_socket.close()
+
+
+def disconnect(connection):
+    connection.send('disconnect'.encode())
+    connection.close()
     sys.exit(0)
 
 
 
 if __name__ == '__main__':
-    receive_thread = Thread(target=recv_msg)
-    thread_send = Thread(target=msg_send)
-    receive_thread.start()
-    thread_send.start()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
