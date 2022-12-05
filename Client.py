2c7ee0766d7309ffebd556fc6a320ef88bdd5682
JLKayser <92865016+JLKayser@users.noreply.github.com>
2022-12-05 11:09:37 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,186 +1,91 @@
-from socket import AF_INET, SOCK_STREAM , socket
-from threading import Thread
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QMainWindow, QComboBox, QDialog, QMessageBox, QTabWidget, QVBoxLayout, QPlainTextEdit, QTextEdit
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
-import time
-from PyQt5.QtCore import Qt
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        widget = QWidget()
-        self.resize(450,300)
-        self.setCentralWidget(widget)
-        grid = QGridLayout()
-        widget.setLayout(grid)
-        self.__tabs = QTabWidget()
-        self.__tab1 = QWidget()
-        self.__tab3 = QWidget()
-        self.__tabs.resize(300,300)
-
-        self.__tabs.addTab(self.__tab1,"Server")
-        self.__tabs.addTab(self.__tab3,"Server-CMD")
-
-        #self.tabs.setStyleSheet("QWidget { background-color: black }")
-        self.__tab1.layout = QGridLayout()
-        self.__tab3.layout = QGridLayout()
-        self.__pushCommand = QLineEdit("")
-        self.__envoie = QPushButton("Send")
-        self.__pushCommand.setPlaceholderText("Type a command...")
-        self.__addressIP = QLineEdit("")
-        self.__addressIP.setPlaceholderText("Type an IP address...")
-        self.__okay = QLabel("Waiting for a connection")
-        self.__port = QLineEdit("")
-        self.__port.setPlaceholderText("Type an port...")
-        self.__connect = QPushButton('Connect')
-        self.__tab1.layout.addWidget(self.__pushCommand)
-        self.__tab1.setLayout(self.__tab1.layout)
-        #self.connect.setStyleSheet("border-radius:5px;background-color:black;color:white;height:20px;width:40px;")
-        self.__text = QLabel("Welcome To my Shell")
-        #self.__text.setReadOnly(True)
-        self.__tab3.layout.addWidget(self.__text)
-        self.__text.move(10, 10)
-        self.__text.resize(400, 200)
-        self.__tab3.setLayout(self.__tab3.layout)
-        #self.__text.setPlaceholderText('Bienvenue sur mon invite de commande !')
-
-
-        grid.addWidget(self.__tabs, 0, 0, 1,2)
-        grid.addWidget(self.__pushCommand, 3, 0)
-        grid.addWidget(self.__envoie, 3, 1)
-        self.__tab1.layout.addWidget(self.__connect, 2, 0)
-        self.__tab1.layout.addWidget(self.__addressIP, 1, 0)
-        self.__tab1.layout.addWidget(self.__port, 1, 1)
-        self.__tab1.layout.addWidget(self.__okay, 2, 1)
-
-
-        self.__connect.clicked.connect(self.__connection)
-        self.__addressIP.returnPressed.connect(self.__connection)
-        self.__port.returnPressed.connect(self.__connection)
-        self.__envoie.clicked.connect(self.__message_send)
-        self.__pushCommand.returnPressed.connect(self.__message_send)
-        self.setWindowTitle("SAE-302")
+import socket, threading
+import sys , os , subprocess
+import psutil
 
 
 
-    def __InValid(self):
-        msg = QMessageBox()
-        msg.setWindowTitle("Not valid")
-        msg.resize(500, 500)
-        msg.setIcon(QMessageBox.Critical)
-        msg.setText("Please enter a valid IP address or port !")
-        msg.exec()
+def cmd(cmd):
+    try:
+        if cmd.lower() == 'os':
+            x = sys.platform
+            if x == 'win32':
+                x = 'OS de la machine est Windows'
+            elif x == 'linux' or x == 'linux2':
+                x = 'OS de la machine est Linux'
+            elif x == 'darwin':
+                x = 'OS de la machine est MAC OS X'
+            return x
+        if cmd.lower() == 'ram':
+            x = str(f'- Total Memory: {psutil.virtual_memory()[0]} bytes\n - Used Memory: {psutil.virtual_memory()[1]} bytes\n - Free Memory: {psutil.virtual_memory()[4]} bytes')
+            return f'RAM:\n {x}'
+        if cmd.lower() == 'cpu':
+            x = psutil.cpu_percent()
+            return f'Capacity CPU: {x} %'
+        if cmd.lower() == 'ip':
+            host = socket.gethostname()
+            x = socket.gethostbyname(host)
+            return x
+        if cmd.lower() == 'name':
+            x = socket.gethostname()
+            return x
+        if cmd.lower() == 'help':
+            return ("""CMD HELP:
+    - IP
+    - NAME
+    - RAM
+    - OS
+    - CPU
+    - KILL
+    - RESET
+    - DISCONNECT""")
+        if cmd[0:4].lower() == 'dos:':
+            x = cmd.split(":",1)[1]
+            out = subprocess.Popen(x, encoding='utf-8', stdout=subprocess.PIPE, shell=True,)
+            output = out.stdout.read()
+            return output
+    except:
+        pass
 
-    def __InValidCommand(self):
-        msg = QMessageBox()
-        msg.setWindowTitle("Not valid")
-        msg.resize(500, 500)
-        msg.setIcon(QMessageBox.Critical)
-        msg.setText("Please enter a valid command !")
-        msg.exec()
+
+class ClientThread(threading.Thread):
+    def __init__(self,clientAddress,clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print ("New connection added: ", clientAddress)
 
 
-    def __connection(self):
-        try:
-            HOST = self.__addressIP.text()
-            PORT = int(self.__port.text())
-            self.socket = connect(HOST,PORT)
-            thread_recu = Thread(target=self.__message_recu)
-            thread_envoie = Thread(target=self.__message_send)
-            thread_recu.start()
-            thread_envoie.start()
-            self.__addressIP.setText("")
-            self.__addressIP.setPlaceholderText("Retype an IP address...")
-            self.__port.setText("")
-            self.__port.setPlaceholderText("Retype an port...")
-            self.__okay.setText("Connection is successful !")
-        except:
-            self.__InValid()
-
-
-    def __message_recu(self):
+    def run(self):
+        print ("Connection from : ", clientAddress)
+        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         while True:
             try:
-                msg = self.socket.recv(1024).decode()
-                self.__text.setText('-> ' + msg)
-                self.__pushCommand.setText("")
-                self.__pushCommand.setPlaceholderText("Retype a command...")
+                data = self.csocket.recv(1024)
+                if data is not None:
+                    msg = data.decode()
+                    if msg.lower() == 'disconnect':
+                        self.csocket.close()
+                        break
+                    if msg.lower() == 'kill':
+                        self.csocket.send("Kill".encode())
+                        server.close()
+                        break
+                    self.csocket.send(cmd(msg).encode())
             except:
-                self.__InValidCommand()
-
-    def __message_send(self):
-        try:
-            msg = self.__pushCommand.text()
-            if msg.lower() == 'reset':
-                self.socket.close()
-                #client_socket = socket(AF_INET, SOCK_STREAM)
-                # new_connection(connection.getpeername())
-                # client_socket.connect((HOST, PORT))
-                # client_socket.sendall(bytes("This is from Client", 'UTF-8'))
-                # msg_send()
-            if msg.lower() != 'disconnect':
-                self.socket.send(msg.encode())
-            else:
-                self.disconnect()
-        except EOFError:
-            pass
-
-    def disconnect(self):
-        self.socket.send('disconnect'.encode())
-        self.socket.close()
-        sys.exit(0)
+                pass
+        print ("Client at ", clientAddress, " disconnected...")
 
 
+PORT = int(input('Please enter a port: '))
+ADDRESS = '0.0.0.0'
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((ADDRESS, PORT))
 
-
-def connect(HOST:str,PORT:int):
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((HOST,PORT))
-    client_socket.sendall(bytes("This is from Client",'UTF-8'))
-    return client_socket
-
-
-'''def msg_send(connection):
-    user = input('Enter your username : ')
-    while True:
-        try:
-            msg = input(f'{user}-> ')
-            if msg == 'reset':
-                connection.close()
-                client_socket = socket(AF_INET, SOCK_STREAM)
-                #new_connection(connection.getpeername())
-                #client_socket.connect((HOST, PORT))
-                #client_socket.sendall(bytes("This is from Client", 'UTF-8'))
-                #msg_send()
-            if msg != 'disconnect':
-                connection.send(msg.encode())
-            else:
-                disconnect(connection)
-        except EOFError:
-            disconnect(connection)'''''
-
-
-def new_connection():
-    pass
-
-
-'''def recv_msg(connection):
-    while True:
-        try:
-            msg = connection.recv(1024).decode()
-            print(msg)
-        except:
-            pass'''
-
-
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec()
+msg = ''
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
